66c4daee1b36867c3d833f7d6094bb600505a874
aungkhant611658 <aungkhant611658@gmail.com>
2024-09-27 20:18:27 +0630

    check auth token

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,9 +1,11 @@
-import { Form, NavLink } from "react-router-dom";
+import { Form, NavLink, useRouteLoaderData } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
+  const token = useRouteLoaderData("root");
+
   return (
     <header className={classes.header}>
       <nav>
@@ -19,6 +21,7 @@ function MainNavigation() {
               Home
             </NavLink>
           </li>
+
           <li>
             <NavLink
               to="/events"
@@ -29,6 +32,7 @@ function MainNavigation() {
               Events
             </NavLink>
           </li>
+
           <li>
             <NavLink
               to="/newsletter"
@@ -39,21 +43,27 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              to="/auth?mode=login"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Authentication
-            </NavLink>
-          </li>
-          <li>
-            <Form action="/logout" method="post">
-              <button>Logout</button>
-            </Form>
-          </li>
+
+          {!token && (
+            <li>
+              <NavLink
+                to="/auth?mode=login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Authentication
+              </NavLink>
+            </li>
+          )}
+
+          {token && (
+            <li>
+              <Form action="/logout" method="post">
+                <button>Logout</button>
+              </Form>
+            </li>
+          )}
         </ul>
       </nav>
       <NewsletterSignup />
